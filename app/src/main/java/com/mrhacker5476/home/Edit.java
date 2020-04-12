89b6ceddfbf15af161480ca66082727e7b826b10
MRTHAKER <mahek.haker@gmail.com>
2020-04-13 00:18:09 +0530

    Local Mate: Add support for Editing Basic details and Optimize Uploading Photos -Fixed photos with larger size was not getting uploaded

--- app/src/main/java/com/mrhacker5476/home/Edit.java
+++ app/src/main/java/com/mrhacker5476/home/Edit.java
@@ -14,6 +14,7 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -34,9 +35,10 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.ResultSet;
 
 
-public class Edit extends AppCompatActivity implements View.OnClickListener,AsyncResponse {
+public class Edit extends AppCompatActivity implements View.OnClickListener {
     ImageView imageView;
     Button upload,update;
     String email;
@@ -44,11 +46,11 @@ public class Edit extends AppCompatActivity implements View.OnClickListener,Asyn
     Bitmap bitmap;
     RegisterBean rb;
     Intent getData;
-    String file="register";
-    String toEn;
+    String file="";
     JSONObject jsonObject;
     HttpURLConnection urlConnection;
     ProgressDialog progressDialog;
+    EditText Email,Phone,FirstName,LastName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +64,15 @@ public class Edit extends AppCompatActivity implements View.OnClickListener,Asyn
         update=findViewById(R.id.ConfirmEdit);
         update.setOnClickListener(Edit.this);
         upload.setOnClickListener(Edit.this);
-        new getImage().execute();
+        Email=findViewById(R.id.EmailEdit);
+        Phone=findViewById(R.id.PhoneEdit);
+        FirstName=findViewById(R.id.FirstNameEdit);
+        LastName=findViewById(R.id.LastNameEdit);
+        progressDialog=new ProgressDialog(Edit.this);
+        progressDialog.setCancelable(false);
+        progressDialog.setTitle("Processing, Please wait...");
+        new getImage().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        new GetRegister().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
     }
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -72,19 +82,19 @@ public class Edit extends AppCompatActivity implements View.OnClickListener,Asyn
             Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
             intent.setType("image/*");
             startActivityForResult(intent,5);
-            rb.setPassword("111");
-            rb.setMobile("111");
-            rb.setGender("gg");
-            rb.setFirstName("55");
-            rb.setLastName("lol");
         }
         if(v==update){
-            new Inner().execute();
+            rb.setMobile(Phone.getText().toString());
+            rb.setFirstName(FirstName.getText().toString());
+            rb.setLastName(LastName.getText().toString());
+            rb.setEmail(Email.getText().toString());
+            new Inner().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+           new UpdateReg().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         }
     }
     public String BitmaptoString(Bitmap b){
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        b.compress(Bitmap.CompressFormat.JPEG,100, stream);
+        b.compress(Bitmap.CompressFormat.WEBP,80, stream);
         byte[] bb=stream.toByteArray();
         Log.d("BBBBBBBBBBBBBBBBBBB",Base64.encodeToString(bb,Base64.URL_SAFE));
         return Base64.encodeToString(bb,Base64.URL_SAFE);
@@ -109,26 +119,137 @@ public class Edit extends AppCompatActivity implements View.OnClickListener,Asyn
             bitmap=null;
         }
     }
-
-    @Override
-    public void processFinish(JSONObject jsonObject) throws JSONException {
-        if(jsonObject.get("done").equals(true)){
-            Toast.makeText(Edit.this,"Success",Toast.LENGTH_SHORT).show();
+    public class GetRegister extends AsyncTask{
+        JSONObject j;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
         }
-        else Toast.makeText(Edit.this,"Error",Toast.LENGTH_SHORT).show();
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            URL url= null;
+            try {
+                url = new URL("http://192.168.43.2:9090/local/register.jsp?display="+email);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection urlConnection = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try{
+
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder a = new StringBuilder();
+                String res;
+                while ((res = bufferedReader.readLine()) != null)
+                    a.append(res);
+                res=a.toString();
+                assert res!=null;
+                j = new JSONObject(res);
+            }
+            catch(Exception e){
+                Log.d("Exception Occurred",e.toString());}
+            finally {
+                urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            Email.setText(email);
+            try {
+                FirstName.setText(j.getString("FirstName"));
+                LastName.setText(j.getString("LastName"));
+                Phone.setText(j.getString("Mobile"));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+    public class UpdateReg extends AsyncTask{
+        JSONObject jj;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            URL url= null;
+            try {
+                url = new URL("http://192.168.43.2:9090/local/register.jsp?update=ok&Email="+rb.getEmail()+"&FirstName="+rb.getFirstName()+"&LastName="+rb.getLastName()+"&Mobile="+rb.getMobile());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection urlConnection = null;
+            try {
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoInput(true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try{
+
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder a = new StringBuilder();
+                String res;
+                while ((res = bufferedReader.readLine()) != null)
+                    a.append(res);
+                res=a.toString();
+                assert res!=null;
+                jj = new JSONObject(res);
+            }
+            catch(Exception e){
+                Log.d("Exception Occurred",e.toString());}
+            finally {
+                urlConnection.disconnect();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            try {
+                if (jj.getBoolean("done")==true){
+                    Toast.makeText(Edit.this,"Perfect",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(Edit.this,"Error",Toast.LENGTH_LONG).show();
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
     public class Inner extends AsyncTask{
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Toast.makeText(Edit.this,"Started",Toast.LENGTH_SHORT).show();
+            progressDialog.show();
         }
 
         @Override
         protected void onPostExecute(Object o) {
             super.onPostExecute(o);
-            Toast.makeText(Edit.this,"Success",Toast.LENGTH_SHORT).show();
+            progressDialog.dismiss();
         }
 
         @Override
@@ -170,19 +291,7 @@ public class Edit extends AppCompatActivity implements View.OnClickListener,Asyn
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            Toast.makeText(Edit.this,"Started",Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        protected void onPostExecute(Object o) {
-            super.onPostExecute(o);
-            Toast.makeText(Edit.this,"Success",Toast.LENGTH_SHORT).show();
-            try {
-                Bitmap bitmapp=DecodeBase64(jsonObject.getString("string"));
-                imageView.setImageBitmap(bitmapp);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+            progressDialog.show();
         }
 
         @Override
@@ -222,5 +331,16 @@ public class Edit extends AppCompatActivity implements View.OnClickListener,Asyn
             }
             return jsonObject;
         }
+        @Override
+        protected void onPostExecute(Object o) {
+            super.onPostExecute(o);
+            progressDialog.dismiss();
+            try {
+                imageView.setImageBitmap(DecodeBase64(jsonObject.getString("string")));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
